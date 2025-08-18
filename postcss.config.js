1dc8ef3f7ccae10ef4d66eb6cdb3b6340093995b
Ewudes <ewudes@gmail.com>
2025-08-18 20:56:26 +0300

    fix: postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -4,10 +4,6 @@ module.exports = {
     require("postcss-easy-import")({
       extensions: ".scss",
     }),
-    require("autoprefixer")({
-      browsers: ["last 2 versions"],
-      cascade: false,
-    }),
     require("postcss-advanced-variables")({
       variables: require("./src/assets/styles/variables"),
     }),

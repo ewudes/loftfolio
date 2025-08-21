742fb29141b632e3fa8dffca59a46efc697522b5
Ewudes <ewudes@gmail.com>
2025-08-21 16:09:06 +0300

    fix: webpack spa config

--- webpack.spa.config.js
+++ webpack.spa.config.js
@@ -1,19 +1,21 @@
-const webpack = require("webpack");
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
 const { VueLoaderPlugin } = require("vue-loader");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
+  mode: isProd ? "production" : "development",
   entry: {
     admin: path.resolve(__dirname, "src/admin/main.js"),
   },
   output: {
     path: path.resolve(__dirname, "dist/admin"),
     filename: "[name].bundle.js",
-    clean: true, // очищает dist при сборке
+    publicPath: "/",
+    clean: true,
   },
   resolve: {
     alias: {
@@ -24,6 +26,19 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.pug$/,
+        loader: "pug-plain-loader",
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
         test: /\.css$/,
         use: ["vue-style-loader", "css-loader"],
       },
@@ -32,53 +47,41 @@ module.exports = {
         use: ["vue-style-loader", "css-loader", "postcss-loader"],
       },
       {
-        test: /\.vue$/,
-        loader: "vue-loader",
-      },
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-      },
-      {
         test: /\.(png|jpg|gif|svg)$/,
         loader: "file-loader",
         options: {
-          name: "[name].[ext]?[hash]",
+          name: "[name].[hash].[ext]",
+          esModule: false,
         },
       },
     ],
   },
   plugins: [
-    new ESLintPlugin({
-      extensions: ["js", "vue"],
-      emitWarning: true,
-    }),
+    new VueLoaderPlugin(),
+    new ESLintPlugin({ extensions: ["js", "vue"], emitWarning: true }),
     new HtmlWebpackPlugin({
       title: "Admin Vue App",
-      filename: "index.html",
       template: path.resolve(__dirname, "src/admin/index.html"),
+      filename: "index.html",
     }),
-    new VueLoaderPlugin(),
-    ...(isProd
-      ? [
-          new webpack.DefinePlugin({
-            "process.env.NODE_ENV": JSON.stringify("production"),
-          }),
-          new webpack.LoaderOptionsPlugin({
-            minimize: true,
-          }),
-        ]
-      : []),
-  ],
+    isProd &&
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+      }),
+  ].filter(Boolean),
   devServer: {
-    historyApiFallback: true,
-    client: {
-      overlay: true,
+    static: {
+      directory: path.join(__dirname, "dist/admin"),
     },
+    port: 8080,
+    historyApiFallback: true,
+    hot: true,
+    open: true,
   },
+  devtool: isProd ? "source-map" : "eval-cheap-module-source-map",
   performance: {
     hints: false,
   },
-  devtool: isProd ? "source-map" : "eval-cheap-module-source-map",
 };
